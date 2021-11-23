fe54067ef3b35eb2a396a5d29597e3897c8cfdbf
Leila <67684543+LeilaZarban@users.noreply.github.com>
2021-11-24 00:05:20 +0100

    make component parametric

--- src/PasswordStrength.tsx
+++ src/PasswordStrength.tsx
@@ -4,23 +4,55 @@ import { PWCountainer, PWDiv } from "./PasswordStrengthStyle";
 
 interface IPWStrength {
   password: string;
+  scoreWords?: string[];
+  colors?: string[];
+  width?: string;
+  height?: string;
+  shapeEdge?: string;
+  backgroundColor?: string;
+  scoreWordClass?: string;
+  onChangeScore?: () => void;
 }
 
 const PasswordStrength: React.FC<IPWStrength> = (props) => {
   const score = props.password && zxcvbn(props.password, []).score;
 
-  const scoreWords = ["Weak", "Medium", "Normal", "Strong", "Very Strong"];
-  const colors = ["#ed5565", "#dd772c", "#f8ac59", "#2b90ef", "#25c281"];
+  const scoreWords = props.scoreWords ?? [
+    "Weak",
+    "Medium",
+    "Normal",
+    "Strong",
+    "Very Strong",
+  ];
+  const colors = props.colors ?? [
+    "#ed5565",
+    "#dd772c",
+    "#f8ac59",
+    "#2b90ef",
+    "#25c281",
+  ];
   const widths = ["10%", "25%", "45%", "70%", "100%"];
 
-  const width = score >= 0 && widths[score];
+  const changingWidth = score >= 0 && widths[score];
   const color = score >= 0 && colors[score];
   const scoreWord = scoreWords[score];
 
   return (
-    <PWCountainer>
-      <PWDiv width={width} color={color}>
-        {scoreWord}
+    <PWCountainer
+      mainwidth={props.width}
+      backgroundColor={props.backgroundColor}
+      height={props.height}
+      shapeEdge={props.shapeEdge}
+    >
+      <PWDiv
+        width={changingWidth}
+        color={color}
+        height={props.height}
+        shapeEdge={props.shapeEdge}
+      >
+        <span className={props.scoreWordClass} onChange={props.onChangeScore}>
+          {scoreWord}
+        </span>
       </PWDiv>
     </PWCountainer>
   );
--- src/PasswordStrengthStyle.tsx
+++ src/PasswordStrengthStyle.tsx
@@ -1,24 +1,34 @@
 import styled from "styled-components";
 
-interface IPWColor {
-    color: string;
-    width: string;
+interface IPW {
+  color: string;
+  width: string;
+  height?: string;
+  shapeEdge?: string;
+}
+interface IPWContainer {
+  mainwidth: string;
+  height?: string;
+  shapeEdge?: string;
+  backgroundColor?: string;
 }
 
 export const PWCountainer = styled.div`
-    border-radius: 4px;
-    background: #ddd;
-    width: 60%;
-    height: 20px;
-    margin: 15px 0px;
+  border-radius: ${(props: IPWContainer) =>
+    (props.shapeEdge = "circle" ? "4px" : "0px")};
+  background: ${(props: IPWContainer) => props.backgroundColor ?? "#ddd"};
+  width: ${(props: IPWContainer) => props.mainwidth ?? "100%"};
+  height: ${(props: IPWContainer) => props.height ?? "20px"};
+  margin: 15px 0px;
 `;
 
 export const PWDiv = styled.div`
-    background: ${(props: IPWColor) => props.color};
-    width: ${(props: IPWColor) => props.width};
-    height: 20px;
-    transition: width 2s;
-    border-radius: 4px;
-    color: #fff;
-    text-align: center;
+  background: ${(props: IPW) => props.color};
+  width: ${(props: IPW) => props.width};
+  height: ${(props: IPW) => props.height ?? "20px"};
+  transition: width 2s;
+  border-radius: ${(props: IPW) =>
+    (props.shapeEdge = "circle" ? "4px" : "0px")};
+  color: #fff;
+  text-align: center;
 `;
